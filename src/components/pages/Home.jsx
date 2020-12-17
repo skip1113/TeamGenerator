a1dd8190b3063d07c0a06d8c329b27e1ce17e87d
Skip Khamvongsa <skipk95@gmail.com>
2020-12-16 20:46:12 -0700

    Added state compnents

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {
+        names: '',
+        date: '',
+        jobs: '',
+        solo: [],
+        duo: [],
+        trio: [],
+        jobNumber: []
+    };
+}
