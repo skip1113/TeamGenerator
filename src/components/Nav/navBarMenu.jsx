e3f443296b36b9e9691fc81744b36c5f541d9a21
Skip Khamvongsa <skipk95@gmail.com>
2020-12-22 18:49:23 -0700

    Installed react bootstrap and created new navbar

--- src/components/Nav/navBarMenu.jsx
+++ src/components/Nav/navBarMenu.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 
 function NavBarMenu() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          Random Team Generator
-        </a>
-        {/* <a className="savedbtn" href="/saved">Saved Teams</a> */}
-      </nav>
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Random Team Generator</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Saved Teams</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+    </Nav>
+    {/* <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form> */}
+  </Navbar>
     );
   }
   
