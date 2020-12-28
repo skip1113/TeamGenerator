b28cb976af4cb88a324c4e2f1fe716832346f8d4
Skip Khamvongsa <skipk95@gmail.com>
2020-12-27 19:28:17 -0700

    Rendered buttons to app

--- src/components/buttons/ButtonList.jsx
+++ src/components/buttons/ButtonList.jsx
@@ -1,40 +1,51 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 // import "./style.css";
 
 // Add or Delete members to list to be assigned
 export function DeleteBtn(props) {
   return (
-    <button className="delete-btn" {...props} onClick={props.onClick} data-id={props.id} tabIndex="0">
-      {/* ✗ */}
-      -
-    </button>
+    // <button className="delete-btn" {...props} onClick={props.onClick} data-id={props.id} tabIndex="0">
+    //   {/* ✗ */}
+    //   -
+    // </button>
+    <Button variant="info">
+      {props.children}
+      Delete
+    </Button>
   );
 }
 
 export function AddBtn(props) {
   return (
-    <button className="add-btn" {...props} onClick={props.onClick} data-id={props.id}>
-      {/* {props.children} */}
-      +
-    </button>
+    // <button className="add-btn" {...props} onClick={props.onClick} data-id={props.id}>
+    //   {/* {props.children} */}
+    //   +
+    // </button>
+    <Button variant="dark">
+      {props.children}
+      Add
+    </Button>
   );
 }
 
 //Add or delete teams/jobs
 export function AddFormBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-secondary">
-      {props.children}
-    </button>
+    // <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-secondary">
+    //   {props.children}
+    // </button>
+    <Button variant="success">Add Job</Button>
   );
 }
 
 export function DeleteFormBtn(props) {
   return (
-    <button {...props} className="btn btn-secondary">
-      {props.children}
-    </button>
-  )
+    // <button {...props} className="btn btn-secondary">
+    //   {props.children}
+    // </button>
+    <Button variant="warning">Delete Job</Button>
+  );
 }
 
 // Handle submit to generate teams and reset/clear fields
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
-import { DeleteBtn, AddBtn } from '../buttons/ButtonList';
+import { DeleteBtn, AddBtn, AddFormBtn, DeleteFormBtn } from '../buttons/ButtonList';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -25,15 +25,18 @@ class Home extends Component {
                         <Col xs={6} className="assign-title">
                             Assignments
                             <InputAssignment />
+                            <AddFormBtn />
+                            <DeleteFormBtn />
                         </Col>
                         <Col xs={6} className="team-title">
                             Colleagues
                             <InputTeam />
+                            <AddBtn />
+                            <DeleteBtn />
                         </Col>
                     </Row>
                 </Container>
-                    <AddBtn />
-                    <DeleteBtn />
+                    
             </div>
         )
     }
