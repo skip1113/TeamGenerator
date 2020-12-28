acffa57bf63306ad3a777983c79e99ec9d6ef3fc
Skip Khamvongsa <skipk95@gmail.com>
2020-12-27 18:15:51 -0700

    Container row col layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { DeleteBtn, AddBtn } from '../buttons/ButtonList'; 
+import { DeleteBtn, AddBtn } from '../buttons/ButtonList';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Home extends Component {
     state = {
@@ -13,10 +16,16 @@ class Home extends Component {
     };
 
     render() {
-        return(
+        return (
             <div>
-                <AddBtn />
-                <DeleteBtn />
+                <Container>
+                    <Row>
+                        <Col xs={6}>1 of 2</Col>
+                        <Col xs={6}>2 of 2</Col>
+                    </Row>
+                </Container>
+                    <AddBtn />
+                    <DeleteBtn />
             </div>
         )
     }
